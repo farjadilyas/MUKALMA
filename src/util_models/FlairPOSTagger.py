c432624914dbd4c22a5f7ecb0871aa7cdb946e14
Farjad Ilyas <ilyasfarjad@gmail.com>
2022-03-29 13:07:47 +0500

    Add Flair's POS Tagger - This tagger outperforms the NLTK POS tagger being used by MUKALMA.   For instance, on the WSJ corpus, it yields an accuracy of 97.9% vs   NLTK's 94% - Practically, replacing NLTK with Flair's POS Tagger in MUKALMA is   useful since it reduces the nummber of False Positives in Noun   Tagging and is more capable of correctly tagging compund words   as Nouns, which is important for keyword extraction from a message - Flair is significantly slower, but the improvement in accuracy   seems worth it

---
+++ src/util_models/FlairPOSTagger.py
@@ -0,0 +1,38 @@
+"""
+  MUKALMA - A Knowledge-Powered Conversational Agent
+  Project Id: F21-20-R-KBCAgent
+
+  Wrapper class for the Flair POS tagger - one of the top ranked POS taggers across multiple metrics
+
+  @Author: Muhammad Farjad Ilyas
+  @Date: 29th March 2022
+"""
+
+from rich.console import Console
+from pathlib import Path
+
+# Flair imports for POS tagging
+import flair
+from flair.data import Sentence
+from flair.models import SequenceTagger
+
+# Change the cache directory for Flair models to one within this project's model directory
+flair.cache_root = Path("../../../models/.flair")
+console = Console(record=True)
+
+
+class FlairPOSTagger:
+    def __init__(self, model_path='flair/pos-english-fast'):
+        self.TAG = 'FlairPOSTagger'
+
+        console.log(f"""[{self.TAG}]: Loading {model_path}...\n""")
+        self.model = SequenceTagger.load(model_path)
+
+    def get_pos_tags(self, message):
+        sentence = Sentence(message)
+        self.model.predict(sentence)
+        toks = sentence.tokens
+        tags = []
+        for idx, entity in enumerate(sentence.get_spans('pos')):
+            tags.append((toks[idx].text, entity.get_labels()[0].value))
+        return tags
