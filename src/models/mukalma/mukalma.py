cb12e7e18ffb666864b82e070ffb4f222c116a38
Farjad Ilyas <ilyasfarjad@gmail.com>
2022-04-14 07:25:12 +0500

    Add input and output text processing - Input text is no longer converted to lowercase to prevent loss of   capitalization information - References to time in input messages are now converted to a concrete   year or range of years to aid the Information Retrieval module - Output text processing, including replacing brackets, both balanced   and imbalanced with commas for more human-like responses, especially   when paired with a text-to-speech module for output text

--- src/models/mukalma/mukalma.py
+++ src/models/mukalma/mukalma.py
@@ -9,7 +9,6 @@
     - This model should be used for generating human-like dialog grounded on knowledge.
 """
 
-
 # Importing Models
 from ...util_models.DialoGPTController import DialoGPTController
 from src.util_models.T5.T5ClozeController import T5ClozeController, getMaskToken
@@ -19,12 +18,139 @@ from ...util_models.FlairPOSTagger import FlairPOSTagger
 from ...util_models.TopicTransitionModel import TopicTransitionModel
 
 # Importing NLU Components
-from .nlu.partsOfSpeech import get_nouns, match_questions, truecasing_by_pos, fix_sentence
+from .nlu.partsOfSpeech import get_nouns, match_questions, fix_sentence
 from .nlu.intentRecognition import IntentRecognizer
+
+# Import packages for text preprocessing of messages
+from nltk.tokenize import word_tokenize
+from .nlu.svo_utils import extract_named_entities, get_lemmatizer
+import dateparser as dp
+from word2number import w2n
+import re
+from time import localtime, strftime
 
 from .KnowledgeSource import KnowledgeSource
 
 from torch import cuda
+
+
+def clean_answer(s):
+    """
+      Replace brackets in answers with commas containing the same text
+    """
+
+    # Match an opening bracket and its preceding character, or a closing bracket and its following character
+    splits = re.split('(.?\(|\).?)', s)
+    cleaned_ans = ""
+
+    # If the split encountered is an opening bracket, introduce a comma before appending its preceding character
+    # append the following character after the comma if the split is a closing bracket
+    # If the split is not a bracket, append the entire split to the cleaned answer
+    # This has the effect of replacing brackets with commas
+    for split in splits:
+        if len(split) >= 2 and split[1] == '(':
+            cleaned_ans = f"{cleaned_ans},{split[0]}"
+        elif len(split) >= 2 and split[0] == ')':
+            cleaned_ans = f"{cleaned_ans},{split[1]}"
+        else:
+            cleaned_ans = f"{cleaned_ans}{split}"
+
+    return cleaned_ans
+
+
+def word_to_num(word):
+    try:
+        return w2n.word_to_num(word)
+    except ValueError:
+        return None
+
+
+# Define keywords used for text processing of strings that contain references to time
+days_of_week = ['saturday', 'sunday', 'monday', 'tuesday', 'wednesday', 'friday']
+future_words = ['next', 'later', 'future']
+past_words = ['ago', 'past', 'previous', 'last']
+range_words = ['past', 'last', 'next', 'within']
+cur_year = int(strftime("%Y", localtime()))
+
+
+def clean_input(s):
+    lemmatizer = get_lemmatizer()
+    p_msg = msg = s
+    nes = extract_named_entities(msg)
+
+    # Replace the named entities referring to dates in the input with date ranges, keep cleaned output in p_msg
+    for ne in nes:
+        if ne[1] != 'DATE':
+            continue
+
+        past = period = unit = None
+        replace = None
+        prev_proc_w = None
+        year_range = False
+
+        # Attempt to parse the named entity tagged as DATE
+        try:
+            dt = dp.parse(ne[0])
+        except:
+            dt = None
+        if dt is not None:
+            replace = str(dt.year)
+        else:
+            # Tokenize and iterate over the words making up the date named entity
+            # This loop aims to extract whether the named entity is talking about the past/present/future
+            # It also extracts the unit of time, and the amount of time mentioned in the named entity
+            words = word_tokenize(ne[0])
+            for w_id, word in enumerate(words):
+                # Normalize words
+                proc_w = lemmatizer.lemmatize(re.sub("'s", '', word.lower()))
+
+                # Compare the normalized word against a set of lists to extract information
+                if proc_w in range_words:
+                    year_range = True
+                if proc_w in days_of_week or proc_w == 'month':
+                    replace = cur_year
+                elif proc_w in past_words:
+                    past = True
+                elif proc_w in future_words:
+                    past = False
+                elif proc_w == 'this':
+                    past = None
+                elif proc_w == 'century':
+                    unit = 100
+                    if prev_proc_w is not None:
+                        period = word_to_num(prev_proc_w)
+                elif proc_w == 'decade':
+                    unit = 10
+                    if prev_proc_w is not None:
+                        period = word_to_num(prev_proc_w)
+                elif proc_w == 'year':
+                    if prev_proc_w is not None:
+                        period = word_to_num(prev_proc_w)
+                    unit = 1
+                prev_proc_w = proc_w
+
+        # Prefix differs based on whether a specific time is mentioned, or if a range is mentioned
+        prefix = "in "
+
+        # Combine the information extracted from this named entity
+        if past is None:
+            replace = str(cur_year)
+        if replace is not None:
+            break
+        if period is None:
+            period = 1
+        if past is not None and period is not None and unit is not None:
+            replace = str(2022 + (-1 if past else 1) * period * unit)
+
+        # Change prefix and replacement text if a range of time is mentioned instead of a specific year
+        if replace is not None and year_range:
+            prefix = "between "
+            replace = f"{replace} - {cur_year}" if int(replace) < cur_year else f"{cur_year} - {replace}"
+
+        # Replace the named entity in the message (p_msg) with the replacement text sent as the second parameter
+        p_msg = re.sub(ne[0], prefix + replace, p_msg)
+
+    return p_msg
 
 
 class MUKALMA:
@@ -33,7 +159,8 @@ class MUKALMA:
 
         # Check for Nvidia CUDA Support on the machine
         cuda.empty_cache()
-        print(f"[{self.TAG}]: __init___: CUDA GPU is {'not' if not cuda.is_available() else ''} available on this machine")
+        print(
+            f"[{self.TAG}]: __init___: CUDA GPU is {'not' if not cuda.is_available() else ''} available on this machine")
 
         # Parameter Configurations
         self.flavor_selected = params["selected_flavor"]
@@ -46,7 +173,7 @@ class MUKALMA:
 
         self.sentence_model = SentenceModel(self.flavor_config["mpnet"], use_cuda=self.cuda_use["mpnet"])
         self.fast_sentence_model = SentenceModel(self.flavor_config["miniLM"], use_cuda=self.cuda_use["miniLM"])
-        
+
         self.intentRecognizer = IntentRecognizer(model_path=self.flavor_config["intent"])
 
         self.dialogue_model = DialoGPTController(self.flavor_config["dialoGPT"], use_cuda=self.cuda_use["dialoGPT"])
@@ -65,10 +192,11 @@ class MUKALMA:
         self.knowledge_db = KnowledgeSource(self.sentence_model.model, num_results=1)
 
         # Responsible for tracking changes in the topic the conversation is centered around
-        self.topic_transition_model = TopicTransitionModel(self.fast_sentence_model.model, use_cuda=self.cuda_use["miniLM"])
+        self.topic_transition_model = TopicTransitionModel(self.fast_sentence_model.model,
+                                                           use_cuda=self.cuda_use["miniLM"])
 
         # ------------------------------------------------------------------------------------------------------------
-        
+
         # Keeps track of the topic that is currently being talked about
         self.topic_str = ""
 
@@ -118,7 +246,7 @@ class MUKALMA:
         # Take the word / phrase retrieved from the knowledge source and complete it
         # This is done by framing this task as a Cloze task
         cloze_responses = self.cloze_model.generate_cloze_responses(
-            f"{message} {getMaskToken(0)} {knowledge_sent} {getMaskToken(1)} .".lower()
+            f"{message} {getMaskToken(0)} {knowledge_sent} {getMaskToken(1)} ."
         )
 
         fragmented_outputs = []
@@ -159,6 +287,7 @@ class MUKALMA:
         outputs[0] = best_response
 
         return best_response, outputs
+
     # End of function
 
     def find_relevant_response(self, message, cur_turn_knowledge, cur_turn_knowledge_tok):
@@ -169,7 +298,7 @@ class MUKALMA:
         if match_nouns_from_set(message, self.knowledge_source_pos) == 0:
             return selected_question, "", -1, -1
         """
-        
+
         # Intent Recognition
         intent = self.intentRecognizer.recognizeIntent(message)
         print(f"[{self.TAG}]: find_relevant_response: Intent: {intent}")
@@ -193,10 +322,10 @@ class MUKALMA:
 
         # If a knowledge_sent couldn't be found, find the most similar sentence instead
         if len(knowledge_sent) == 0:
-            
-            print(f"[{self.TAG}]: find_relevant_response: SENT TOK KNOWLEDGE: { cur_turn_knowledge_tok }")
+
+            print(f"[{self.TAG}]: find_relevant_response: SENT TOK KNOWLEDGE: {cur_turn_knowledge_tok}")
             print(f"\n\n[{self.TAG}]: find_relevant_response: SENT TOK KNOWLEDGE:\n{cur_turn_knowledge_tok[:4]}\n\n")
-            print ("[{self.TAG}]: find_relevant_response: message:\n{message}")
+            print("[{self.TAG}]: find_relevant_response: message:\n{message}")
 
             knowledge_sents, knowledge_start_index, knowledge_end_index = \
                 self.sentence_model.get_most_similar_sentence(message, cur_turn_knowledge_tok), -1, -1
@@ -213,7 +342,8 @@ class MUKALMA:
 
                 # Finding all the questions for each noun we identify in the message
                 message_keyword = " ".join(message_nouns)
-                knowledge_questions.extend(self.question_generation_model.generate_questions(knowledge_sent, message_keyword))
+                knowledge_questions.extend(
+                    self.question_generation_model.generate_questions(knowledge_sent, message_keyword))
                 print(f"[{self.TAG}]: find_relevant_response: Questions Generated: {knowledge_questions}")
 
                 # Select the most relevant question from the list of generated questions
@@ -237,10 +367,13 @@ class MUKALMA:
         # End if
 
         # Logging and Return
-        print(f"[{self.TAG}]: find_relevant_response: Response: {knowledge_sent} start span: {knowledge_start_index} end span: {knowledge_end_index}")
+        print(
+            f"[{self.TAG}]: find_relevant_response: Response: {knowledge_sent} start span: {knowledge_start_index} end span: {knowledge_end_index}")
         return selected_question, knowledge_sent, knowledge_start_index, knowledge_end_index
 
     def get_response(self, message):
+        message = clean_input(message)
+
         # Get a list of topics that may have been mentioned in the message
         # Add keywords from conversation history if they're relevant, and update the topic transition model
         topics = self.topic_transition_model.update_topic(message, self.__extract_topic(message))
@@ -266,17 +399,17 @@ class MUKALMA:
             self.generate_knowledge_based_response(message, knowledge_sent)
 
         # Cleaning the responses
-        best_response = truecasing_by_pos(best_response)
+        best_response = clean_answer(best_response)
         for i in range(len(knowledge_grounded_responses)):
-            knowledge_grounded_responses[i] = truecasing_by_pos(knowledge_grounded_responses[i])
+            knowledge_grounded_responses[i] = clean_answer(knowledge_grounded_responses[i])
 
         # Logging and Return
         print(f"[{self.TAG}]: get_response: GENERATED RESPONSE: {knowledge_grounded_responses}")
         return {
             "knowledge_sent": knowledge_sent,
-            "response": best_response, 
+            "response": best_response,
             "candidates": knowledge_grounded_responses,
             "knowledge_source": cur_turn_knowledge,
-            "k_start_index": knowledge_start_index, 
+            "k_start_index": knowledge_start_index,
             "k_end_index": knowledge_end_index
         }
