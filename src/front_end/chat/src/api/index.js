a6a8e04fd2af57a10079369e8b6222a8a02cb9d7
Nabeel Danish <nabeelben@gmail.com>
2022-04-18 06:42:39 +0500

    [Removed]: KnowledgeRetriever and UI calls
    - Removed knowledge_retrieval module from api
    - Removed fetchSource, fetchTopic, changeTopic calls from ui
    - Removed ui functionality for searching topics

--- src/front_end/chat/src/api/index.js
+++ src/front_end/chat/src/api/index.js
@@ -4,8 +4,3 @@ const API = axios.create({ baseURL: "https://97f2-206-84-140-3.ngrok.io" });
 
 export const sendMessage = (message) => API.post('/reply', message)
 export const waitForResponse = () => API.get('/get_update')
-
-export const fetchSource = () => API.get('/source')
-export const fetchTopics = () => API.get('/topics')
-
-export const changeTopic = (topic) => API.post('/topics/select', topic)
