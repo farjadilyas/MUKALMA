0f9479c91da7546b2d2013f2d2bbc7f146d2929f
Nabeel Danish <nabeelben@gmail.com>
2022-04-20 10:56:27 +0500

    [Chat-frontend]: Updating URL fixed

--- src/front_end/src/api/index.js
+++ src/front_end/src/api/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: "http://3fb7-35-238-228-239.ngrok.io" });
+export var API = axios.create({ baseURL: "http://3fb7-35-238-228-239.ngrok.io" });
+export const updateApiUrl = (url) => {
+    API = axios.create({ baseURL: url });
+}
 
 export const sendMessage = (message) => API.post('/reply', message)
 export const waitForResponse = () => API.get('/get_update')
